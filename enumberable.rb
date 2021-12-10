de9a9359f714c0d4f29ca687005b2dd8929513f9
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-12-10 13:16:31 +0200

    added filter method

--- enumberable.rb
+++ enumberable.rb
@@ -1,39 +1,17 @@
 module MyEnumberable
-  def all?(*params)
-    result = true
-    if !params[0].nil?
-      each do |item|
-        result = false unless params[0] === item
-      end
-    elsif !block_given?
-      each do |item|
-        result = false unless item
-      end
-    else
-      each do |item|
-        result = false unless yield(item)
-      end
-    end
-    result
+  def all?
+    each { |item| return false unless yield item }
+    true
   end
 
-  def any?(*params)
-    result = false?
-      if !params[0].nil?
-        each do |item|
-          result = true if params[0] === item
-        end
-      elsif !block_given?
-        each do |item|
-          result = true if item
-        end
-      else
-        each do |item|
-          result = true if yield(item)
-        end
-      end
-      result
+  def any?
+    each { |item| return true if yield item }
+    false
   end
 
- 
+  def filter
+    results = []
+    each { |item| results << element if yield item }
+    results
+  end
 end
