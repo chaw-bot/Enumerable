ddac408552e2f5528af771e65b50d59ed507866f
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-12-10 14:23:52 +0200

    Refactor code

--- main.rb
+++ main.rb
@@ -3,19 +3,29 @@ require_relative './enumberable'
 class MyList
   include MyEnumberable
 
-  def initialize(*list)
-    @list = list
+  def initialize(*items)
+    @list = []
+    items.each { |item| @list << item }
   end
 
-  attr_reader :list
+  def each
+    return to_enum(:each) unless block_given?
+
+    i = 0
+    while i < @list.length
+      yield @list[i]
+      i += 1
+    end
+    @list
+  end
 end
 
 list = MyList.new(1, 2, 3, 4)
 
-puts(list.list.all? { |e| e > 5 })
-puts(list.list.all? { |e| e > 5 })
+puts(list.all? { |e| e > 5 })
+puts(list.all? { |e| e > 5 })
 
-puts(list.list.any? { |e| e == 2 })
-puts(list.list.any? { |e| e == 5 })
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
 
-puts(list.list.filter(&:even?))
+p list.filter(&:even?)
