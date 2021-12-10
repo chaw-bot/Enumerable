7806fc84e32f4a6609f34a3ec83059b84bfb030f
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-12-10 10:58:50 +0200

    Verify your solution of the all? and any? methods

--- main.rb
+++ main.rb
@@ -18,5 +18,8 @@ list = MyList.new(1, 3, 5)
 
 # p list.list
 
+puts [1, 2, 3, 4].all? {|e| e > 5}
+puts list.list.all? {|e| e > 5}
 
-
+puts list.list.any? {|e| e == 2}
+puts list.any? {|e| e == 5}
