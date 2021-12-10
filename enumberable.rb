37e726b1f23fe634fe41d76ab37668fecdab7d4d
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-12-10 10:57:36 +0200

    Create a module MyEnumerable that implements the any? method

--- enumberable.rb
+++ enumberable.rb
@@ -16,3 +16,21 @@ module MyEnumberable
     end
     result
   end
+
+  def any?(*params)
+    result = false?
+      if !params[0].nil?
+        each do |item|
+          result = true if params[0] === item
+        end
+      elsif !block_given?
+        each do |item|
+          result = true if item
+        end
+      else
+        each do |item|
+          result = true if yield(item)
+        end
+      end
+      result
+  end
